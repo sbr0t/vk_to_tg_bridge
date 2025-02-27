69ee60d38f21434871c87fd0adcec134c99964e3
sbr0t <eisinyakov@gmail.com>
2025-02-27 16:33:22 +0300

    v0.1-alpha

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
+import os
+import time
 import vk_api
-from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+import telebot
 import requests
 from configs import *
+from telebot import types
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+
 
 vk_session = vk_api.VkApi(token=VK_API_KEY)
-longpoll = VkBotLongPoll(vk_session, '229561055')
-
+longpoll = VkBotLongPoll(vk_session, VK_GROUP_ID)
+bot = telebot.TeleBot(TG_BOT_TOKEN)
 
 def download_file(url, filename):
 	response = requests.get(url)
 	if response.status_code == 200:
 		with open(filename, 'wb') as f:
 			f.write(response.content)
-		print(f'Файл сохранён как {filename}')
 	else:
 		print(f'Ошибка скачивания: {response.status_code}')
 
@@ -20,7 +24,6 @@ def download_file(url, filename):
 for event in longpoll.listen():
 	if event.type == VkBotEventType.MESSAGE_NEW:
 		message = event.message
-		print('Текст:', message.text)
 
 		# Проверяем, есть ли вложения
 		attachments = message.attachments if hasattr(message, 'attachments') else []
@@ -48,5 +51,43 @@ for event in longpoll.listen():
 					if url:
 						download_file(url, DOWNLOAD_FOLDER + filename)
 				# Можно добавить обработку и для других типов вложений, например, video, audio и т.д.
-		else:
-			print("Нет вложений в сообщении.")
+
+		files = os.listdir(DOWNLOAD_FOLDER)
+
+		if files:
+			photo_count = 0
+			for file in files:
+				photo_count += 1 if '.jpg' in file else 0
+
+			if photo_count == 1 and len(files) == 1:
+				with open(DOWNLOAD_FOLDER + files[0], 'rb') as photo:
+					bot.send_photo(TG_CHANEL_ID, photo, caption= message.text)
+
+			elif photo_count == len(files):
+				photos = [open(DOWNLOAD_FOLDER + files[0], 'rb')]
+				media = [
+					types.InputMediaPhoto(photos[0], caption=message.text),
+				]
+
+				for i, file in enumerate(files[1:]):
+					photos.append(open(DOWNLOAD_FOLDER + file, 'rb'))
+					media.append(types.InputMediaPhoto(photos[i+1]))
+
+				bot.send_media_group(TG_CHANEL_ID, media)
+
+				for photo in photos:
+					photo.close()
+
+			elif len(files) == 1:
+				with open(DOWNLOAD_FOLDER + files[0], 'rb') as doc:
+					bot.send_document(TG_CHANEL_ID, doc, caption=message.text)
+
+			else:
+				with open(DOWNLOAD_FOLDER + files[0], 'rb') as doc:
+					bot.send_document(TG_CHANEL_ID, doc, caption=message.text)
+				for file in files[1:]:
+					with open(DOWNLOAD_FOLDER + file, 'rb') as doc:
+						bot.send_document(TG_CHANEL_ID, doc)
+
+			for file in files:
+				os.remove(DOWNLOAD_FOLDER + file)
